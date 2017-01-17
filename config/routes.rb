39760e1a85c35e664c3ab1314f797b609c1b8e94
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-17 22:39:01 +0900

    Modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events, param: :title
   resources :users, param: :username, only: %i(index show)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
