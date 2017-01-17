9f44aa33dfce6086e47bb0f9c40b2d507bc33c3a
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-17 09:22:17 +0900

    Change param name in users routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :events
-  resources :users, only: %i(index show)
+  resources :users, param: :username, only: %i(index show)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
