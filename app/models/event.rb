8c4844216e39995e90cfe51335b06a42e885c994
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-20 16:04:41 +0900

    Add validations to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: { scope: :scheduled_at }
+  validates :scheduled_at, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :events
 
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true
+
   def to_param
     username
   end
