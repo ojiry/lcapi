315d36ac2eabf7551e9d2feef37857bc61531021
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-20 19:29:10 +0900

    Implement participates and declines

--- app/controllers/events/declines_controller.rb
+++ app/controllers/events/declines_controller.rb
@@ -1,4 +1,18 @@
 class Events::DeclinesController < ApplicationController
+  before_action :set_event, only: :destroy
+  before_action :set_user, only: :destroy
+
   def destroy
+    @participation = @user.participations.find_by(event_id: @event.id)
+    @participation&.destroy
   end
+
+  private
+    def set_event
+      @event = Event.find(params[:event_id])
+    end
+
+    def set_user
+      @user = User.find_by!(username: params[:username])
+    end
 end
--- app/controllers/events/participates_controller.rb
+++ app/controllers/events/participates_controller.rb
@@ -1,4 +1,23 @@
 class Events::ParticipatesController < ApplicationController
+  before_action :set_event, only: :create
+  before_action :set_user, only: :create
+
   def create
+    @participation = @user.participations.new(event: @event)
+
+    if @participation.save
+      render json: @participation, status: :created
+    else
+      render json: @participation.errors, status: :unprocessable_entity
+    end
   end
+
+  private
+    def set_event
+      @event = Event.find(params[:event_id])
+    end
+
+    def set_user
+      @user = User.find_by!(username: params[:username])
+    end
 end
