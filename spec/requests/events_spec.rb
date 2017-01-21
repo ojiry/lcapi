1ef3a1e07874b8c90233f5ac876198212fb62e5c
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-21 14:07:50 +0900

    Add events#create spec

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -2,18 +2,34 @@
 require 'rails_helper'
 
 RSpec.describe "Events", type: :request do
+  let(:headers) do
+    {
+      'Authorization': "Token #{Rails.configuration.x.access_token}",
+      'Content-Type': 'application/json'
+    }
+  end
+
+  subject { response }
+
   describe "GET /events" do
-    let(:headers) do
-      {
-        'Authorization': "Token #{Rails.configuration.x.access_token}",
-        'Content-Type': 'application/json'
-      }
-    end
-
-    subject { response }
-
     before { get events_path, headers: headers }
 
     it { is_expected.to have_http_status(200) }
+  end
+
+  describe "POST /events" do
+    let(:user) { User.create email: "testuser@example.com", username: "testuser" }
+
+    let(:params) do
+      {
+        name: "testevent",
+        scheduled_at: Time.current,
+        username: user.username
+      }.to_json
+    end
+
+    before { post events_path, params: params, headers: headers }
+
+    it { is_expected.to have_http_status(201) }
   end
 end
