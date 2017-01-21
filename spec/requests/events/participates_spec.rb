df65bd88683144da4e73ab940e16310632235ea4
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-21 14:12:52 +0900

    Add request spec of events/participates api

---
+++ spec/requests/events/participates_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Events::Participates", type: :request do
+  describe "POST /events/:event_id/participates" do
+    let(:user) { User.create email: "testuser@example.com", username: "testuser" }
+    let(:event) { user.events.create name: "testevent", scheduled_at: Time.current }
+    let(:params) { { username: user.username }.to_json }
+
+    let(:headers) do
+      {
+        'Authorization': "Token #{Rails.configuration.x.access_token}",
+        'Content-Type': 'application/json'
+      }
+    end
+
+    subject { response }
+
+    before { post event_participate_path(event), params: params, headers: headers }
+
+    it { is_expected.to have_http_status(201) }
+  end
+end
