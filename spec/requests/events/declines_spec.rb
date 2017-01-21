a35b3a9f06100dc08c1834aff50ce6f874285e52
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-21 14:16:28 +0900

    Add request spec of events/declines api

---
+++ spec/requests/events/declines_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Events::Declines", type: :request do
+  describe "DELETE /events/:event_id/declines" do
+    let(:user) { User.create email: "testuser@example.com", username: "testuser" }
+    let(:event) { user.events.create name: "testevent", scheduled_at: Time.current }
+    let(:params) { { username: user.username }.to_json }
+
+    let(:headers) do
+      {
+        'Authorization': "Token #{Rails.configuration.x.access_token}",
+        'Content-Type': 'application/json'
+      }
+    end
+
+    subject { response }
+
+    before { delete event_decline_path(event), params: params, headers: headers }
+
+    it { is_expected.to have_http_status(204) }
+  end
+end
