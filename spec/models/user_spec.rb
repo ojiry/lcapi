be1feda92f8f801e4d2f71418e4777ba6eaf7171
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-21 13:06:35 +0900

    Generate model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  let(:user) { User.new email: "testuser@example.com", username: "testuser" }
+
+  describe "#to_param" do
+    subject { user.to_param }
+
+    it { is_expected.to eq user.username }
+  end
+end
