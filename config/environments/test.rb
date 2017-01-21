ee7ca56dee627f65aae7ce9b3e363c2906e62aaf
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-21 13:25:58 +0900

    Add request spec of users api

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,4 +40,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.x.access_token = 'lcapi_test_access_token'
 end
